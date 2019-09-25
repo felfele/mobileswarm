5cc5e52a8fdaedd96537658c14736b2dea5332de
Attila Gazso <agazso@gmail.com>
2019-09-25 20:12:43 +0200

    Removed Wait from StartNode

--- lib/library.go
+++ lib/library.go
@@ -170,7 +170,6 @@ func StartNode(path, listenAddr, cBootnodeURL, loglevel string) string {
 		return "error 3: " + err.Error()
 	}
 
-	swarmNode.Wait()
 	log.Info("----------- node started ---------------")
 	return fmt.Sprintf("%v", account.Address.Hex())
 }
